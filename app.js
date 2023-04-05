c418ccabea992e2370a35985c73285c675277d6a
Ian Selby <ianselbyuk@gmail.com>
2023-04-05 13:03:52 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,175 @@
+// UI elements
+const modal = document.querySelector(".modal");
+const addBookForm = document.querySelector("#bookForm");
+const inputTitle = document.querySelector("#title");
+const inputAuthor = document.querySelector("#author");
+const inputPages = document.querySelector("#pages");
+const inputRead = document.querySelector("#read");
+const cardContainer = document.querySelector(".card-container");
+
+// event listeners
+document.querySelector("#addBook").addEventListener("click", openModal);
+document.querySelector(".btn-close").addEventListener("click", closeModal);
+addBookForm.addEventListener("submit", addNewBook);
+cardContainer.addEventListener("click", handleClick);
+
+let myLibrary = [];
+
+// Book Constructor
+function Book(id, title, author, pages, read) {
+  this.id = id;
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+}
+
+// initial demo data
+const demoData = [
+  {
+    id: 0,
+    title: "The Hobbit",
+    author: "J.R.R. Tolkien",
+    pages: 290,
+    read: true,
+  },
+  { id: 1, title: "Dad Jokes", author: "U.B. Laffing", pages: 50, read: true },
+  {
+    id: 2,
+    title: "Lost in Time",
+    author: "A Seiko",
+    pages: 178,
+    read: false,
+  },
+];
+
+// push demo data into myLibrary as book objects
+myLibrary = demoData.map(
+  ({ id, title, author, pages, read }) =>
+    new Book(id, title, author, pages, read)
+);
+
+// openModal
+function openModal() {
+  modal.classList.remove("hidden");
+}
+
+// closeModal
+function closeModal() {
+  modal.classList.add("hidden");
+}
+
+// addNewBook
+function addNewBook(e) {
+  e.preventDefault();
+  const id = myLibrary[myLibrary.length - 1].id + 1;
+  const newBook = new Book(
+    id,
+    inputTitle.value,
+    inputAuthor.value,
+    inputPages.value,
+    inputRead.checked
+  );
+  myLibrary.push(newBook);
+  displayCard(newBook);
+  clearForm();
+  closeModal();
+}
+
+function clearForm() {
+  inputTitle.value = "";
+  inputAuthor.value = "";
+  inputPages.value = "";
+  inputRead.checked = false;
+}
+
+function deleteBook(id) {
+  const card = document.querySelector(`[data-id="${id}"]`);
+  console.log(`[data-id="${id}"]`);
+  card.remove();
+
+  // delete from the library
+  myLibrary.splice(id, 1);
+}
+
+function handleClick(e) {
+  const id = e.target.closest(".card").getAttribute("data-id");
+  if (e.target.parentElement.classList.contains("delete-button")) {
+    deleteBook(id);
+  }
+  if (e.target.parentElement.classList.contains("toggle")) {
+    toggleReadStatus(id);
+  }
+}
+
+function displayCard(book) {
+  const card = document.createElement("article");
+  card.setAttribute("data-id", book.id);
+  card.classList.add("card");
+
+  const cardHeading = document.createElement("header");
+  cardHeading.classList.add("card-header");
+  card.appendChild(cardHeading);
+
+  const title = document.createElement("h3");
+  title.textContent = book.title;
+  cardHeading.appendChild(title);
+
+  const cardContent = document.createElement("div");
+  cardContent.classList.add("card-content");
+  card.appendChild(cardContent);
+
+  const authorHeading = document.createElement("span");
+  authorHeading.textContent = "Author";
+  authorHeading.classList.add("card-content-heading");
+  cardContent.appendChild(authorHeading);
+
+  const author = document.createElement("p");
+  author.textContent = book.author;
+  cardContent.appendChild(author);
+
+  const pagesHeading = document.createElement("span");
+  pagesHeading.textContent = "Pages";
+  pagesHeading.classList.add("card-content-heading");
+  cardContent.appendChild(pagesHeading);
+
+  const pages = document.createElement("p");
+  pages.textContent = book.pages;
+  cardContent.appendChild(pages);
+
+  const cardFooter = document.createElement("footer");
+  cardFooter.classList.add("card-footer");
+  card.appendChild(cardFooter);
+
+  const toggleLabel = document.createElement("label");
+  toggleLabel.classList.add("toggle");
+  cardFooter.appendChild(toggleLabel);
+
+  const labelForToggle = document.createElement("span");
+  labelForToggle.classList.add("toggle-label");
+  labelForToggle.textContent = "Read?";
+  toggleLabel.appendChild(labelForToggle);
+
+  const toggleCheckbox = document.createElement("input");
+  toggleCheckbox.setAttribute("type", "checkbox");
+  toggleCheckbox.classList.add("toggle-checkbox");
+  book.read
+    ? (toggleCheckbox.checked = true)
+    : (toggleCheckbox.checked = false);
+  toggleLabel.appendChild(toggleCheckbox);
+
+  const toggleSwitch = document.createElement("div");
+  toggleSwitch.classList.add("toggle-switch");
+  toggleLabel.appendChild(toggleSwitch);
+
+  const deleteBookButton = document.createElement("button");
+  deleteBookButton.setAttribute("type", "button");
+  deleteBookButton.classList.add("delete-button");
+  deleteBookButton.innerHTML = "<i class='bi bi-trash3'></i>";
+  cardFooter.appendChild(deleteBookButton);
+
+  cardContainer.appendChild(card);
+}
+
+console.log(myLibrary);
+myLibrary.forEach(displayCard);
